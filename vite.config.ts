a428dce0e070e8ed97545a2da3aefe4dc21d14a8
RageNeko26 <rneko2006@gmail.com>
2023-09-01 00:16:44 +0700

    feat: Add Proxy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api" : {
+        target: "http://ec2-3-113-2-184.ap-northeast-1.compute.amazonaws.com/api/v1/auth",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      }
+    }
+  }
 })
